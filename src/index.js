323a07a6ae480b8780b244ce970b59a8251bc08a
James Brumond <kbjr14@gmail.com>
2015-01-20 16:50:23 -0800

    better error handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 
+var fs            = require('fs');
+var path          = require('path');
 var Promise       = require('promise-es6').Promise;
 var childProcess  = require('child_process');
 
 exports.clone = function(opts, callback) {
 	return new Promise(function(resolve, reject) {
+		if (exists(opts)) {
+			return reject('error: ' + opts.target + ' already exists');
+		}
 		childProcess.exec(cmd(opts), {cwd: process.cwd()}, function(err, stdout, stderr) {
 			if (err) {
 				return reject(err);
@@ -22,4 +27,8 @@ function cmd(opts) {
 	var branch = opts.branch ? ' --branch ' + opts.branch : '';
 
 	return 'git clone --depth 1' + branch + ' git@github.com:' + opts.owner + '/' + opts.repo + target;
+}
+
+function exists(opts) {
+	return fs.existsSync(path.join(process.cwd(), opts.target));
 }
